e0690a906e12a3648616e6175caa6751d44f41ae
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-20 20:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+   return num.toString(2);
 }
-
+const num = parseInt(prompt("Enter a number."));
+alert(decimalToBinary(num));
 window.decimalToBinary = decimalToBinary;
